016776100361c7869bd58d8cf023b722d84dc661
MiguelRebequi <130229587+MiguelRebequi@users.noreply.github.com>
2026-03-30 09:52:29 -0300

    Commit inicial do projeto atividade7

---
+++ src/main/java/com/miguel/atividade6/model/Produto.java
@@ -0,0 +1,43 @@
+package com.miguel.atividade6.model;
+
+public class Produto {
+
+    // Construtor vazio (necessário para o Spring Boot)
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    private Long id;
+    private String nome;
+    private double preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
